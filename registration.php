222d1178ad9ccde3150eba2bd34f236af8d9c47e
Sebastian Keutmeier <sebastian@keutmeier.de>
2021-03-11 11:56:59 +0100

    error fix

--- registration.php
+++ registration.php
@@ -1,6 +1,9 @@
 <?php
-        \Magento\Framework\Component\ComponentRegistrar::register(
-            \Magento\Framework\Component\ComponentRegistrar::MODULE,
-            'Skeutmeier_Customemailvar',
-            __DIR__
-        );
+/**
+ * Copyright © SKeutmeier BV All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Skeutmeier_Customemailvar', __DIR__);
+
